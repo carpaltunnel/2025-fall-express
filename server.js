08154f3d89e011b9e01ff83916c475bf32a06c59
CC <a_fake_address@uca.edu>
2025-09-01 20:29:38 -0500

    chicken controller

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 import express from 'express';
+import { ChickenController } from './controllers/chickens.controller.js';
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
+//ChickenController.getChickens();
+
+app.get('/api/v1/chickens', ChickenController.getChickens);
+app.get('/api/v1/chickens/:id', ChickenController.getChicken);
+app.post('/api/v1/chickens', ChickenController.createChicken);
+app.put('/api/v1/chickens/:id', ChickenController.replaceChicken);
+app.patch('/api/v1/chickens/:id', ChickenController.updateChicken);
+app.delete('/api/v1/chickens/:id', ChickenController.deleteChicken);
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Chicken API is listening at http://localhost:${port}`);
 });
